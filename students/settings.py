78872086853e626fa692c0443bc94c57478b061d
porkstand <62133460+porkstand@users.noreply.github.com>
2020-05-01 14:52:40 +0800

    Update settings.py

--- students/settings.py
+++ students/settings.py
@@ -35,9 +35,9 @@ from linebot.models import *
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'Ub8e3cf75739079f25a50f82b2cbd4c63'
 # 必須放上自己的Channel Access Token
-LINE_CHANNEL_ACCESS_TOKEN ='528gEzSdPX8Pw9mTJlfpXRpTBdETmaT0LJuHs8+3/qBsiz1lWzBRIfWXQexgOdiPyGxeLtL9HAggCjAp0LBJcqKAdv7h8Ux5JQpKMioyobyYoBQpwer/eA3cA1e89/b80sl219M34lvpXlAAFud++wdB04t89/1O/w1cDnyilFU='
+LINE_CHANNEL_ACCESS_TOKEN ='ysxvoQjcMrkskgiuFZPbLhZs6cD8KCQBCuZ+dbtoy4ZhoIFF9VEgrkfdkY3eXL1dn8SMZj8lwOkXt0tjgQeKOeugzDv7RLq32w4RBS9kf7AdqgZ/Bp8K+MPW8/i049bDFL3zJz9EdreINqk8/vnYvAdB04t89/1O/w1cDnyilFU='
 # 必須放上自己的Channel Secret
-LINE_CHANNEL_SECRET='43664ae683e1af5eb58087c85b74216d'
+LINE_CHANNEL_SECRET='19b59cfc9db35c6d0221dfe1e650aec7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 ALLOWED_HOSTS = ['*']
